27142dce2d0e57bc8c56c777e5fb0d181e8d4d44
sunlt77 <103977593+sunlt77@users.noreply.github.com>
2025-07-16 14:55:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,3 +3,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
+#更改主机名
+sed -i "s/hostname='.*'/hostname='LinWrt'/g" package/base-files/files/bin/config_generate
